650708ccd4d975682dd412fe65a5a4234d8b25f4
AswinAnand66 <aswinananad66@gmail.com>
2022-06-08 12:34:34 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
 
 
 
+
+
+
+app.get('/',function (req,res){
+    res.send("Hello Express")
+})
 console.log("Hello World")
-
-
 
 
 
